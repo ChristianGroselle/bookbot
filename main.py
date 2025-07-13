cb7e1ccd5730b57b9b318f1f67e0fd8339908868
ChristianGroselle <christian.groselle@gmail.com>
2025-07-13 19:43:48 -0400

    refactored the util functions to stats.py

--- main.py
+++ main.py
@@ -1,32 +1,7 @@
 # Christian Groselle Boot dev BookBot
 
-def get_book_text(filepath):
-    """
-    Reads the content of a book file and returns it as a string.
-    Args:
-        filepath (str): The path to the book file.
-
-    Returns:
-        str: The content of the book file.
-    """
-
-    # Read the file and return the contents as a string
-    with open(filepath) as f:
-        return f.read()
-
-def count_words(input_str):
-    """
-    Reads a string and returns the number of words it contains
-    Args:
-        input_str (str): The string to count.
-
-    Returns:
-        int: The number of words in the string.
-    """
-
-    words = input_str.split()
-
-    return len(words)
+from stats import get_book_text
+from stats import count_words
 
 def main():
     book_content = get_book_text("books/frankenstein.txt")
